b17f39893fb6022b9c02dcf66538b9a7def0ea18
CodingBruh123 <90079242+CodingBruh123@users.noreply.github.com>
2021-09-21 09:38:57 +0530

    Update rate.js

--- commands/fun/rate.js
+++ commands/fun/rate.js
@@ -4,12 +4,11 @@ const { Color } = require("../../config.js");
 
 module.exports = {
   name: "rate",
-  aliases: [],
+  aliases: ["mark"],
   description: "Bot Rate Your Given Thing!",
   usage: "Rate <Text>",
   run: async (client, message, args) => {
     //Start
-    message.delete();
     let Content = args.join(" ");
 
     if (!Content)
@@ -18,6 +17,7 @@ module.exports = {
     let embed = new Discord.MessageEmbed()
       .setColor(Color)
       .setTitle(`I Rate`)
+      .setThumbnail("https://media.giphy.com/media/Pr8gkPaFlGKNnd8OcW/giphy.gif?cid=790b76110f46ce849de29d1f7b1c471c763a23c10c810333&rid=giphy.gif&ct=g")
       .setDescription(`${Math.floor(Math.random() * 11)}/10 To ${Content}`)
       .setFooter(`Requested by ${message.author.username}`)
       .setTimestamp();
