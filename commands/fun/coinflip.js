1b9d51ae0f531b56ac3e28c3e63b57ca852f6575
CodingBruh123 <90079242+CodingBruh123@users.noreply.github.com>
2021-09-21 09:36:57 +0530

    Update coinflip.js

--- commands/fun/coinflip.js
+++ commands/fun/coinflip.js
@@ -4,19 +4,19 @@ const { Color } = require("../../config.js");
 
 module.exports = {
   name: "coinflip",
-  aliases: ["toss", "flip"],
+  aliases: ["toss", "cf"],
   description: "Flip A Coin!",
   usage: "Coinflip",
   run: async (client, message, args) => {
     //Start
-    message.delete();
     const coins = ["Heads", "Tails", "Center"];
 
     let result = Math.floor(Math.random() * coins.length);
 
     const embed = new MessageEmbed()
       .setColor(Color)
-      .setTitle(`Coin Is`)
+      .setTitle(`The Coin Is <a:p_coin:888697192267800607>`)
+      .setThumbnail("https://cdn.dribbble.com/users/31664/screenshots/1377335/coin-flip.gif")
       .setDescription(coins[result])
       .setFooter(`Fliped by ${message.author.username}`)
       .setTimestamp();
